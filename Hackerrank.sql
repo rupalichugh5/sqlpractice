c31035de905f8023552600dde690cb91596c44de
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-06-27 05:37:25 +0000

    Hackerrank

--- Hackerrank.sql
+++ Hackerrank.sql
@@ -15,3 +15,21 @@ BEGIN
     PRINT replicate(' *',@Counter)
     SET @Counter  = @Counter - 1
 END
+
+/*
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+Write a query to print the pattern P(20).
+*/
+DECLARE @Counter INT 
+SET @Counter = 1
+WHILE ( @Counter <= 20 )
+BEGIN
+    PRINT replicate(' *',@Counter)
+    SET @Counter  = @Counter + 1
+END
