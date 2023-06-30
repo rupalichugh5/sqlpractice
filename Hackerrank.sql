02a53de0be3c79a1ed866797fb8b0e39b990d1d2
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-06-30 13:47:10 +0000

    2023-06-30

--- Hackerrank.sql
+++ Hackerrank.sql
@@ -33,3 +33,14 @@ BEGIN
     PRINT replicate(' *',@Counter)
     SET @Counter  = @Counter + 1
 END
+
+
+/*
+A median is defined as a number separating the higher half of a data set from the lower half. 
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+*/
+select cast(round(((SELECT MAX(Lat_N) FROM
+   (SELECT TOP 50 PERCENT Lat_N FROM Station ORDER BY Lat_N) AS BH)
+   +
+   (SELECT Min(Lat_N) FROM
+   (SELECT TOP 50 PERCENT Lat_N FROM Station ORDER BY Lat_N desc) AS TH))/2,4) as decimal(6,4)) as median
