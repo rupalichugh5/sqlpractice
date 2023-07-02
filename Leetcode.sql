3a48bd875b9e32f24ff3071143e23d3f1d078f99
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-07-02 05:55:11 +0000

    leetcode

--- Leetcode.sql
+++ Leetcode.sql
@@ -42,3 +42,27 @@ Output:
 select user_id,  concat(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2,Length(name)))) as name
 from users
 order by user_id;
+
+/*
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write an SQL query to report the patient_id, patient_name and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+Return the result table in any order.
+*/
+SELECT * from patients
+where conditions like 'DIAB1%' OR conditions like '% DIAB1%'
+
+SELECT * from patients
+where regexp_like(conditions,' +DIAB1|^DIAB1')
+
