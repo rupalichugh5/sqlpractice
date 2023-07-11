1383d2b95e20746a7ee59a1bfca0b9fe6b1bd9ef
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-07-11 18:59:17 +0000

    Leetcode

--- Leetcode.sql
+++ Leetcode.sql
@@ -66,3 +66,33 @@ where conditions like 'DIAB1%' OR conditions like '% DIAB1%'
 SELECT * from patients
 where regexp_like(conditions,' +DIAB1|^DIAB1')
 
+
+/*
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write an SQL query to report the fraction of players that logged in again on the day after the day they first logged in,
+ rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive 
+ days starting from their first login date, then divide that number by the total number of players.
+*/
+with cte as (select player_id, min(event_date) as start
+from activity
+group by player_id)
+
+select round(count(distinct c.player_id )/(select count(distinct player_id) from activity),2) as fraction
+from cte c
+join activity a
+on c.player_id = a.player_id
+and datediff(c.start, a.event_date) = -1
