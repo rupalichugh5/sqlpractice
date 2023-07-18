79e0b4c50d48d6a0ef6a24d51320301804ebe54c
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-07-18 17:52:04 +0000

    leetcode

--- Leetcode.sql
+++ Leetcode.sql
@@ -133,3 +133,27 @@ group by machine_id
 select machine_id, round(sum(if(activity_type = 'start', -timestamp, timestamp))/count(distinct process_id),3) as processing_time
 from activity
 group by machine_id
+
+
+/*
+Table: Scores
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| score       | decimal |
++-------------+---------+
+In SQL, id is the primary key for this table.
+Each row of this table contains the score of a game. Score is a floating point value with two decimal places.
+ 
+
+Find the rank of the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+
+*/
+select score, dense_rank() over(order by score desc ) as 'rank' from scores
