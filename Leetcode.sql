dc1b39d04732d3ecc690e6c617805093aa1ffdf3
rupalichugh5 <136044801+rupalichugh5@users.noreply.github.com>
2023-07-18 19:22:13 +0000

    leetcode - 182

--- Leetcode.sql
+++ Leetcode.sql
@@ -212,3 +212,26 @@ from employee e
 join employee m
 on e.id = m.managerId
 where e.salary<m.salary
+
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+*/
+select email
+from person
+group by email
+having count(email)>1
+
